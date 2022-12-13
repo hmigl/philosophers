2b2c8cdfc521c1e770a462c75d1d4cc4ce4db1e9
hmigl <88746406+hmigl@users.noreply.github.com>
2022-12-13 15:39:50 -0300

    feat: store command-line arguments

--- philo/main.c
+++ philo/main.c
@@ -9,9 +9,25 @@ static int	rtfm(void)
 	return (1);
 }
 
+static void	store_dinner_info(t_dinner *dinner, char **argv)
+{
+	*dinner = (t_dinner){
+		.nb_of_philosophers = ft_atoi(argv[0]),
+		.time_to_die = ft_atoi(argv[1]),
+		.time_to_eat = ft_atoi(argv[2]),
+		.time_to_sleep = ft_atoi(argv[3])
+	};
+	dinner->max_meals = -2147483648;
+	if (argv[4])
+		dinner->max_meals = ft_atoi(argv[4]);
+}
+
 int	main(int argc, char *argv[])
 {
+	t_dinner	dinner;
+
 	if (!has_valid_arguments(argc, argv + 1))
 		return (rtfm());
+	store_dinner_info(&dinner, argv + 1);
 	return (0);
 }
