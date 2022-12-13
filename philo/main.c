00b50985a2bd63c4c5830cf1d12367488328c994
hmigl <88746406+hmigl@users.noreply.github.com>
2022-12-13 14:37:39 -0300

    feat: on error, return 'how to use' instructions

--- philo/main.c
+++ philo/main.c
@@ -1,8 +1,17 @@
 #include "philo.h"
 
+#define RTFM "usage: ./philo number_of_philosophers time_to_die time_to_eat\
+ time_to_sleep [max_meals]\n"
+
+static int	rtfm(void)
+{
+	write(STDERR_FILENO, RTFM, 88);
+	return (1);
+}
+
 int	main(int argc, char *argv[])
 {
 	if (!has_valid_arguments(argc, argv + 1))
-		return (1);
+		return (rtfm());
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -2,6 +2,7 @@
 # define PHILO_H
 
 # include <stdlib.h>
+# include <unistd.h>
 
 typedef struct s_philo_dinner	t_dinner;
 
