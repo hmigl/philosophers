063f3130c36678f3acef7c3cd1034ca48bc633fc
hmigl <88746406+hmigl@users.noreply.github.com>
2022-12-13 13:58:42 -0300

    feat: create main struct

--- philo/philo.h
+++ philo/philo.h
@@ -1,4 +1,12 @@
 #ifndef PHILO_H
 # define PHILO_H
 
+struct s_philo_dinner {
+	int	nb_of_philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	max_meals;
+};
+
 #endif // !PHILO_H
