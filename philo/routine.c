f7d58bb00a9bef179e2106d66f5dcb8c69602640
hmigl <88746406+hmigl@users.noreply.github.com>
2022-12-20 16:05:12 -0300

    feat: design 'start_routine' loop

--- philo/routine.c
+++ philo/routine.c
@@ -1,6 +1,20 @@
 #include "philo.h"
 
+static int	still_not_satisfied(t_philo *philo)
+{
+	return (philo->meals < philo->dinner->max_meals);
+}
+
 void	*start_routine(void *arg)
 {
+	t_philo	*philo;
+	int		eating_nonstop;
+
+	philo = (struct s_philo *)arg;
+	eating_nonstop = philo->dinner->max_meals < 0;
+	while (eating_nonstop || (!eating_nonstop && still_not_satisfied(philo)))
+	{
+		;
+	}
 	return (NULL);
 }
